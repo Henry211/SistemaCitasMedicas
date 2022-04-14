c2ee7636847d34f1cc602bb45dfaf127253e1fb2
MariaCruzC <maria.cruz@est.una.ac.cr>
2022-04-13 19:08:45 -0600

    creación clase Databases

--- proyecto1P4/proyecto1P4/script.sql
+++ proyecto1P4/proyecto1P4/script.sql
@@ -30,7 +30,30 @@ insert into administrador(cedula,password,nombre) values("343","man3","manuel");
 
 --registro medico
 
-create table medico(cedula varchar(10) not null, password varchar(30) not null,
-nombre varchar(30) not null,Primary Key (password));
+create table medico(nombre varchar(30) not null, cedula varchar(10) not null,
+clave varchar(30) not null,costo_consulta double,Primary Key (clave));
+
+insert into medico(nombre,cedula,clave, costo_consulta) values("samuel","345","samudoc",1200);
+insert into medico(nombre,cedula,clave, costo_consulta) values("antony","555","ant1",3000);
+
+
+--registro especialidad
+create table especialidad(id int not null,nombre varchar(30) not null,
+Primary Key (id));
+
+insert into especialidad(id,nombre) values(1,"pediatra");
+
+--tabla cita falta meter el usuario 
+
+create table cita(id int not null, estado varchar(30) not null, fecha date,
+Primary Key (id));
+
+--registro ciudad
+create table ciudad(id int not null, provincia varchar(30) not null,
+canton varchar(30) not null,distrito varchar(30) ,Primary Key (id));
+
+insert into ciudad(id,provincia,canton,distrito) values(1,"san jose","acosta","mora");
+insert into ciudad(id,provincia,canton,distrito) values(2,"san jose","desamparados","san miguel");
+
 
 
--- proyecto1P4/proyecto1P4/src/main/java/citas/logic/Especialidad.java
+++ proyecto1P4/proyecto1P4/src/main/java/citas/logic/Especialidad.java
@@ -9,5 +9,55 @@ package citas.logic;
  * @author ESCINF
  */
 public class Especialidad {
+    int id;
+    String nombre;
+
+    public Especialidad() {
+    }
+
+    public Especialidad(int id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 23 * hash + this.id;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Especialidad other = (Especialidad) obj;
+        return this.id == other.id;
+    }
+    
+    
     
 }
