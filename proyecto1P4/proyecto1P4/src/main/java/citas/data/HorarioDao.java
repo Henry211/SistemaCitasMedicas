71938311e62713b8d7c6f08adee3dcea4377153c
MariaCruzC <maria.cruz@est.una.ac.cr>
2022-04-28 15:14:15 -0600

    Clase HorarioDao

--- proyecto1P4/proyecto1P4/src/main/java/citas/data/HorarioDao.java
+++ proyecto1P4/proyecto1P4/src/main/java/citas/data/HorarioDao.java
@@ -31,9 +31,20 @@ public class HorarioDao {
         String sql = "insert into horario(id_doctor,comienzo_lunes,finaliza_lunes,comienzo_martes,finaliza_martes,comienzo_miercoles,finaliza_miercoles,comienzo_jueves,finaliza_jueves,comienzo_viernes,finaliza_viernes,comienzo_sabado,finaliza_sabado,frecuencia) "
                 + "values(?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
         PreparedStatement stm = db.prepareStatement(sql);
-//        stm.setString(1, u.getIdmedico().getCedula());
-//        stm.setInt(2, u.getDia());
-        
+        stm.setString(1, u.getMedico().getCedula());
+        stm.setInt(2, u.getIniLunes());
+        stm.setInt(3, u.getFinLunes());
+        stm.setInt(4, u.getIniMartes());
+        stm.setInt(5, u.getFinMartes());
+        stm.setInt(6, u.getIniMiercoles());
+        stm.setInt(7, u.getFinMiercoles());
+        stm.setInt(8, u.getIniJueves());
+        stm.setInt(9, u.getFinJueves());
+        stm.setInt(10, u.getIniViernes());
+        stm.setInt(11, u.getFinViernes());
+        stm.setInt(12, u.getIniSabado());
+        stm.setInt(13, u.getFinSabado());
+        stm.setInt(14, u.getFrecuencia());
         int count = db.executeUpdate(stm);
         if (count == 0) {
             throw new Exception("Horario ya existe");
@@ -55,14 +66,22 @@ public class HorarioDao {
     }
      
              
-    Horarios from(ResultSet rs, String alias){
+    Horario from(ResultSet rs, String alias){
         try {
-            Horarios c= new Horarios();
-            c.setNumero(rs.getInt(alias+".numero"));
-            c.setIdmedico((Medico) rs.getObject(alias+".id_doctor"));
-            c.setDia(rs.getInt(alias+".day"));
-            c.setHora_comienzo(rs.getInt(alias+".hora_comienzo"));
-            c.setHora_finaliza(rs.getInt(alias+".hora_finaliza"));
+            Horario c= new Horario();
+            c.setMedico((Medico) rs.getObject(alias+".id_doctor"));
+            c.setIniLunes(rs.getInt(alias+"comienzo_lunes"));
+            c.setFinLunes(rs.getInt(alias+"finaliza_lunes"));
+            c.setIniMartes(rs.getInt(alias+"comienzo_martes"));
+            c.setFinMartes(rs.getInt(alias+"finaliza_martes"));
+            c.setIniMiercoles(rs.getInt(alias+"comienzo_miercoles"));
+            c.setFinMiercoles(rs.getInt(alias+"finaliza_miercoles"));
+            c.setIniJueves(rs.getInt(alias+"comienzo_jueves"));
+            c.setFinJueves(rs.getInt(alias+"finaliza_jueves"));
+            c.setIniViernes(rs.getInt(alias+"comienzo_viernes"));
+            c.setFinViernes(rs.getInt(alias+"finaliza_viernes"));
+            c.setIniViernes(rs.getInt(alias+"comienzo_sabado"));
+            c.setFinViernes(rs.getInt(alias+"finaliza_sabado"));
             c.setFrecuencia(rs.getInt(alias+".frecuencia"));
             return c;
         } catch (SQLException ex) {
